cd531c228288aba0b4936393bdb2d8433461eb6c
tianwen <yaohii@foxmail.com>
2026-04-02 21:12:33 +0800

    Update Consts.cs

--- Engine/Source/Core/Consts.cs
+++ Engine/Source/Core/Consts.cs
@@ -5,9 +5,11 @@ namespace Engine.Core;
 public static partial class Const
 {
     public static Vector2Int _4K = new Vector2Int(3840, 1920);
-    public static Vector2Int _2K = new Vector2Int(2160, 1440);
-    public static Vector2Int _720P = new Vector2Int(1280, 720);
+    public static Vector2Int _2K = new Vector2Int(2560, 1440);
     public static Vector2Int _1080P = new Vector2Int(1920, 1080);
+    public static Vector2Int _720P = new Vector2Int(1280, 720);
+    public static Vector2Int _540P = new Vector2Int(960, 540);
+    public static Vector2Int _360P = new Vector2Int(640, 360);
 
     public static Color DefaultColor = new Color(0x3A3A3A);
 }
